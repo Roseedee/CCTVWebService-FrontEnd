d9aebec92598261a1182989dc76a2e3c13c34db6
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-06-01 22:16:01 +0700

    update header

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const port = 80
 app.set ( "view engine", "ejs" );
 
 app.use(express.static(path.join(__dirname, 'styles')));
+app.use(express.static(path.join(__dirname, '../static')));
 
 app.get('/', (req, res) => {
   res.render(__dirname + "/routes/")
