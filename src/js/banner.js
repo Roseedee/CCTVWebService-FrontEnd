81d2e431799917cfa12f0fdc3f6b5e3f25d85621
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-06-14 22:27:03 +0700

    optimize

--- src/js/banner.js
+++ src/js/banner.js
@@ -1,36 +1,22 @@
 setInterval(slideBanner, 1500); //show image daley
 
-const banner_img_name = [
-    '1.jpg',
-    '2.jpg',
-    '3.jpg',
-    '4.png',
+const banner_img_name = [ //image data
+    'img-test-banner.png',
+    'img-test-banner1.png',
+    'img-test-banner2.png'
 ]
 
-let img_index = 0;
-
-const img_position_container = document.querySelector('.img-position ul')
-
-banner_img_name.forEach((img_name) => {
-    img_position_container.innerHTML += '<li class="img-position-item"></li>'
-})
-
-const img_position_items = document.querySelectorAll('.img-position-item');
+let slideIndex = 0;
 
 function slideBanner() {
-    reset_img_position();
-    if(img_index >= 4) {
-        img_index = 0;
-    }
-    // const banner_img_item = document.querySelectorAll('.banner-img-item');
-    const banner_items = document.getElementById('banner-items');
-    banner_items.innerHTML = `<img class="banner-img-item" src="/` + banner_img_name[img_index] + `">`
-    img_position_items[img_index].innerHTML = '<div></div>';
-    img_index++;
-}
 
-function reset_img_position() {
-    img_position_items.forEach((item) => {
-        item.innerHTML = '';
-    })
+    const banner_items = document.getElementById('banner-items');
+
+    if(slideIndex >= banner_img_name.length) {
+        slideIndex = 0;
+    }
+
+    banner_items.innerHTML = `<img class="banner-img-item fade" src="/banner/` + banner_img_name[slideIndex] + `">`;
+
+    slideIndex++;
 }
