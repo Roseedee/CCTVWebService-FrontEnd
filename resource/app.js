017796622ed6925650ee7bc43615b6f5e7965cb2
Roseedee Cehleah <49389805+Roseedee@users.noreply.github.com>
2024-07-19 16:24:52 +0700

    update route

--- resource/app.js
+++ resource/app.js
@@ -10,7 +10,9 @@ const customer_route = require('./routes/route.customer.js');
 
 app.set ( "view engine", "ejs" );
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
 
 //route rule
 app.use('/', basic_route);
